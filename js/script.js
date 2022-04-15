69241daa0cb34f74a5e46cc8b5615df9ce020379
Ponja94 <henrique.danielb@gmail.com>
2022-04-15 14:40:41 -0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /* picking a team */
 var team = ''
 var spotId = ''
+var counterGamePlayed = 0
+var matrixGame = [[0,0,0],[0,0,0],[0,0,0]]
 
 /* btn team blue */
 $('[action-click1],[action-click2]').on('click', function(e){
@@ -19,12 +21,14 @@ $('[action-click3]').on('click', function(e){
 	$('[action-click1],[action-click2]').show()
 	
 	$('.spot-peca-jogada').children().remove()
+
+	matrixGame = [[0,0,0],[0,0,0],[0,0,0]]
 	location.reload()
 })
 
 /* verifica de onde veio o clique */
 $(document).on("click", "[action-chose11], [action-chose12], [action-chose13], [action-chose21], [action-chose22], [action-chose23], [action-chose31], [action-chose32], [action-chose33]", function (e) {
-	spotId = this.parentNode.id;
+	spotId = this.parentNode.id
 	selectXorO()
 
 })
@@ -37,15 +41,40 @@ function selectXorO(){ /* tenho que receber (time,posição)*/
 
 		$('#'+spotId).html('<img class="xis" src="img/xis.png">')
 		team = 'btn-r'
+		markingPoints()
 
 	}else if(team == 'btn-r'){
 
 		$('#'+spotId).html('<img class="bola" src="img/bola.png">')
 		team = 'btn-b'
+		markingPoints()
 
 	}else{
 		alert('Pick a team')
 	}
 }
 
+/* contando jogadas e aplicando vencedores */
+function markingPoints(){
+	counterGamePlayed += 1
 	
+	if(counterGamePlayed >= 1){ /* alterar para 5 */
+		console.log('veio')
+		
+		$.each($('.spot-peca-jogada img'), function(i, e) {
+		  /*console.log(i + ' : ' + this.className + ' : ' + this.parentNode.id)*/
+		  
+		  var positionClass = this.parentNode.id
+		  var pecaJogada = this.className
+
+		  console.log(positionClass + pecaJogada)
+
+		})
+
+
+	}else{
+
+	}
+
+
+}
